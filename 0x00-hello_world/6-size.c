ec1850a4e6c60a7f08d950a57c9d781c18f3c5fd
Morayo11 <usmankolawole42@gmail.com>
2022-09-12 21:30:18 -0700

    finished some projects

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - main block
+ * return - return 0
  */
 int main(void)
 {
@@ -9,5 +10,5 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-	return 0;
+	return (0);
 }
