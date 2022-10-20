2d7af29c3d50a3a8393911df978a8da0312ec809
Morayo11 <usmankolawole42@gmail.com>
2022-10-20 13:12:51 -0700

    made some changes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,5 +1,7 @@
-#include "lists.h"
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
  * add_node - Adds a new node at the beginning
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * free_list - Frees a list_t list.
